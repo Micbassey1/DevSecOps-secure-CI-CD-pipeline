20628f66ccec179f0e87dc136001830b0f1c9434
Micbassey1 <michaelbassey808@gmail.com>
2026-06-09 15:16:09 +0100

    Semgrep detection

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+password = "admin123"
+
 @app.route("/")
 def home():
     return "DevSecOps Security Pipeline"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
